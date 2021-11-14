9c8a57cd6249c508bed796aff88baeb64944cf7a
Mohamed Diaa <72948823+MohamedDiaaEldin@users.noreply.github.com>
2021-11-15 00:01:24 +0200

    Update models.py

--- models.py
+++ models.py
@@ -8,9 +8,6 @@ if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 
-
-
-
 print('database url ---------> ', DATABASE_URL)
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -25,51 +22,6 @@ def setup_db(app):
     db.create_all()
     return db
 
-'''
-Customers
-	id (pr), first_name , last_name , address , phone_number , total_cart
-
-
-Cart items :
-	id (pr), quantity,  product_id (fk), customer_id (fk)
-
-
-Categories:
-	id (pr), category_name
-
-Products :
-	id (pr), product_name  , category (fk)
-
-
-'''
-# class Category(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     category_name = db.Column(db.String, nullable=False)
-#     products = db.relationship('Product', backref='products')
-
-# class Product(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     product_name = db.Column(db.String, nullable=False)    
-#     products = db.relationship('cart_item', backref='items')
-#     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-
-# class Customer(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String, nullable=False)
-#     last_name = db.Column(db.String, nullable=False)
-#     address = db.Column(db.String, nullable=False)
-#     phone = db.Column(db.String, nullable=False)
-#     total_price = db.Column(db.Float)    
-#     customers = db.relationship('cart_item', backref='customer')
-
-# class CartItem(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     quantity = db.Column(db.Integer)
-#     ## foreign keys
-#     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-#     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
-    
-    
 
 class Users(db.Model):
     email = db.Column(db.String,  primary_key=True)
